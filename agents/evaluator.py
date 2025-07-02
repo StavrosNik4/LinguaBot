3bce5eea9ad3e46324e1da18926035103776ceb1
StavrosNik4 <stavniko@csd.auth.gr>
2025-07-02 15:26:23 +0300

    Created Question-Answer-Evaluation mockup flow

--- agents/evaluator.py
+++ agents/evaluator.py
@@ -1,3 +1,26 @@
 from typing import Dict, TypedDict
 from langgraph.graph import StateGraph
 
+
+class AgentState(TypedDict):
+    question: str
+    answer: str
+    evaluation: str
+
+
+def evaluate_node(state: AgentState) -> AgentState:
+    """Simple node that evaluates user's answer"""
+
+    state["evaluation"] = "evaluation"
+
+    return state
+
+
+graph = StateGraph(AgentState)
+
+graph.add_node("evaluate", evaluate_node)
+
+graph.set_entry_point("evaluate")
+graph.set_finish_point("evaluate")
+
+app = graph.compile()
--- tools/discord_bot.py
+++ tools/discord_bot.py
@@ -3,7 +3,7 @@ import asyncio
 from dotenv import load_dotenv
 import os
 
-from agents import examiner
+from agents import examiner, evaluator
 
 load_dotenv()
 
@@ -21,16 +21,19 @@ number_of_questions = 5
 # Tracking user progress
 question_index = 0
 
+latest_question = ""
+
 
 @client.event
 async def on_ready():
-    global question_index
+    global question_index, latest_question
     print(f'Logged in as {client.user}')
     user = await client.fetch_user(TARGET_USER_ID)
 
     try:
         result = examiner.app.invoke({})
         await user.send(result["question"])
+        latest_question = result["question"]
         question_index = 1  # Next question to ask
     except Exception as e:
         print(f"Failed to send first question: {e}")
@@ -38,16 +41,22 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    global question_index
+    global question_index, latest_question
     if message.author.bot:  # if message from evaluator
         return
 
     # Only handle DMs from the target user
     if isinstance(message.channel, discord.DMChannel) and message.author.id == TARGET_USER_ID:
 
+        if latest_question != "":
+            answer = message
+            evaluator_result = evaluator.app.invoke({'question': latest_question, 'answer': answer})
+            await message.channel.send(evaluator_result["evaluation"])
+
         if question_index < number_of_questions:
-            result = examiner.app.invoke({})
-            await message.channel.send(result["question"])
+            examiner_result = examiner.app.invoke({})
+            await message.channel.send(examiner_result["question"])
+            latest_question = examiner_result["question"]
             question_index += 1
         else:
             await message.channel.send("Thanks for answering all the questions!")
